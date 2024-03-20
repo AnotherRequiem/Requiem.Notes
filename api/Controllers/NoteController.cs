fa7e7e745ac74431dbcc63c20063fdc9c3172dcf
Requiem <onerequiem@yahoo.com>
2024-03-20 20:46:53 +0200

    Note controller added

---
+++ api/Controllers/NoteController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Data;
+
+namespace WebApi.Controllers;
+
+[Route("api/note")]
+public class NoteController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public NoteController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var notes = _context.Notes.ToList();
+
+        return Ok(notes);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById([FromRoute] Guid id)
+    {
+        var note = _context.Notes.Find(id);
+
+        if (note == null)
+            return NotFound();
+
+
+        return Ok(note);
+    }
+}
